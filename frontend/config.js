361b0864e75999ab571c93714b3ecd9060e81660
Joao Pedro <joaopedropavanelo@gmail.com>
2026-07-10 19:48:07 -0300

    alterações frontend

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+const API = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? '/api'
+  : 'https://api-agendamento-barbearia-1-production.up.railway.app/api';
